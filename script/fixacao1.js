ecf1f2a8d59e77931447afba61702482640e912b
Diana Fonseca <87833140+dianafonseeca@users.noreply.github.com>
2021-09-16 22:38:01 -0300

    Add files via upload

---
+++ script/fixacao1.js
@@ -0,0 +1,19 @@
+var nome = null;
+nome = "fonseca";
+console.log (nome);
+
+var nasc = 1998;
+nasc = "outubro";
+console.log (nasc);
+
+let cidade = "recife";
+console.log(cidade);
+
+let sigla_estado = "PE";
+console.log ("sigla_estado: " + sigla_estado);
+
+const salario_fixo = 1000;
+console.log ("meu salário fixo: " + salario_fixo);
+
+const sorte = 7;
+console.log ("meu número da sorte é " + sorte);
---
+++ script/fixacao2.js
@@ -0,0 +1,27 @@
+//number
+var idade = 23;
+console.log ("idade: " + idade);
+
+//string 
+var nome_sobrenome = "diana fonseca";
+console.log ("nome_sobrenome: " + nome_sobrenome);
+
+//boolean 
+var esta_animada = false;
+console.log ("esta_animada? " + esta_animada);
+
+//array
+var series_preferidas = [" aot", " this is us", " modern family"];
+console.log ("series_preferidas: " + series_preferidas);
+
+//object 
+var ruel = {nome: "ruel", idade: 18, mes_aniversario: "outubro", otimo_cantor: true};
+console.log (ruel);
+
+//undefined
+var sem_valor;
+console.log ("sem_valor: " + sem_valor)
+
+//null
+var vazio= null;
+console.log ("vazio: " + vazio);
